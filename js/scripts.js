7862a7f5136ccc5b6403ef82e2de1e454fbd6395
Jason Brown <jasontbrown99@gmail.com>
2017-01-11 08:23:01 -0800

    entered initial variable setup in scripts. Also changed family-names id to family

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+
+    var person1Input = $("input#person1").val();
+    var verb1Input = $("input#verb1").val();
+    var placeInput = $("input#place").val();
+    var nounsInput = $("input#nouns").val();
+    var familyInput = $("input#family").val();
+
     $(".person1").append();
     $(".verb1").append();
     $(".place").append();
