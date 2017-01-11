2dd3f6c042fdd8cafbc32821cd6152f7c9b9887f
Jason Brown <jasontbrown99@gmail.com>
2017-01-11 08:38:54 -0800

    submitting push for review.  js code not working.  grr

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-
     var person1Input = $("input#person1").val();
     var verb1Input = $("input#verb1").val();
     var placeInput = $("input#place").val();
     var nounsInput = $("input#nouns").val();
     var familyInput = $("input#family").val();
 
-    $(".person1").append();
-    $(".verb1").append();
-    $(".place").append();
-    $(".nouns").append();
-    $(".family-names").append();
+    $(".person1").text(person1Input);
+    $(".verb1").text(verb1Input);
+    $(".place").text(placeInput);
+    $(".nouns").text(nounsInput);
+    $(".family").text(familyInput);
 
     $("#story").show();
 
     event.preventDefault();
   });
-
 });
